8ef2800be3cc1963732fe6437f46166b42801bf6
Noel C <noelchetty@gmail.com>
2021-04-25 17:01:46 +0800

    Minor format changes

--- PG_FSD_1/src/projectFileManagement/WelcomeScreen.java
+++ PG_FSD_1/src/projectFileManagement/WelcomeScreen.java
@@ -48,7 +48,7 @@ public class WelcomeScreen {
 		// Lets use users default directory for demo
 		String current = System.getProperty("user.home");
 		
-		current += "/test"; // For testing etc..remove before submission.
+		current += "/test/test"; // For testing etc..remove before submission.
 		
 	  	System.out.println ("\nYour Current Home Dir : "+ current);
 	  	
@@ -171,7 +171,7 @@ public class WelcomeScreen {
 					}while (choice !=7);
 				
 				System.out.print ("\nReturning to main menu\n\n");
-				WelcomeScreen.welcome();
+				//WelcomeScreen.welcome();
 				break;
 		
 			case 3: // Exit 
